61fb804da628fce455d49ab6f5cf2bc0e46232b2
vilic <i@vilic.info>
2018-08-13 22:52:19 +0800

    Allow filtering tests to run

--- tslint-rules/src/test/runner.ts
+++ tslint-rules/src/test/runner.ts
@@ -3,10 +3,13 @@ import * as Path from 'path';
 import * as glob from 'glob';
 import {consoleTestResultHandler, runTest} from 'tslint/lib/test';
 
+let pattern = process.argv[2];
+
 console.info('Testing Lint Rules:');
 
 let testDirs = glob
   .sync('../../test/rules/**/tslint.json', {cwd: __dirname, absolute: true})
+  .filter(path => !pattern || path.includes(pattern))
   .map(path => Path.dirname(path));
 
 let allPassed = true;
