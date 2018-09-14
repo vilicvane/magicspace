adb798fb9979c42116503430b944e193922f5415
HaoDaWang <1306036576@qq.com>
2018-09-15 00:05:36 +0800

    Update language server

--- vscode-magicspace/server/src/server.ts
+++ vscode-magicspace/server/src/server.ts
@@ -1,3 +1,41 @@
-function sayHello(){
+import {
+  ProposedFeatures,
+  TextDocuments,
+  createConnection,
+} from 'vscode-languageserver';
 
-}
+let connection = createConnection(ProposedFeatures.all);
+
+let textDocuments = new TextDocuments();
+
+connection.onInitialize(_params => {
+  return {
+    capabilities: {
+      textDocumentSync: textDocuments.syncKind,
+    },
+  };
+});
+
+// textDocuments.onDidChangeContent(change => {
+//   let document: TextDocument = change.document;
+
+//   let text = document.getText();
+
+//   let diagnostic: Diagnostic = {
+//     severity: DiagnosticSeverity.Error,
+//     range: {
+//       start: document.positionAt(0),
+//       end: document.positionAt(text.length),
+//     },
+//     message: text,
+//   };
+
+//   connection.sendDiagnostics({
+//     uri: document.uri,
+//     diagnostics: [diagnostic],
+//   });
+// });
+
+textDocuments.listen(connection);
+
+connection.listen();
