bd83a6aca4caf1fadec72cc1d37fac54ac4f1d9c
HaoDaWang <1306036576@qq.com>
2018-09-15 22:15:42 +0800

    Import language client and update struct of code

--- vscode-magicspace/client/src/extension.ts
+++ vscode-magicspace/client/src/extension.ts
@@ -1,60 +1,9 @@
-import * as Path from 'path';
+import {ExtensionContext} from 'vscode';
 
-import {
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-  TransportKind,
-} from 'vscode-languageclient';
-
-import {ExtensionContext, window, workspace} from 'vscode';
-
-let client: LanguageClient;
+import {InsertCommand} from './commands';
+import {createLanguageClient} from './language-client';
 
 export function activate(context: ExtensionContext): void {
-  let serverModule = context.asAbsolutePath(
-    Path.join('server', 'bld', 'server.js'),
-  );
-
-  let debugOptions = {execArgv: ['--nolazy', '--inspect=6009']};
-
-  let serverOptions: ServerOptions = {
-    run: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-    },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      options: debugOptions,
-    },
-  };
-
-  let clientOptions: LanguageClientOptions = {
-    documentSelector: [
-      {
-        scheme: 'file',
-        language: 'typescript',
-      },
-    ],
-    synchronize: {
-      fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
-    },
-  };
-
-  client = new LanguageClient(
-    'magicspaceLanguageServer',
-    'magicspace language server',
-    serverOptions,
-    clientOptions,
-  );
-
-  context.subscriptions.push(client.start());
-
-  window.showInformationMessage('language server is started !!');
-  // context.subscriptions.push(new InsertCommand());
-}
-
-export function deactivate(): Thenable<void> | undefined {
-  return client ? client.stop() : undefined;
+  context.subscriptions.push(createLanguageClient(context).start());
+  context.subscriptions.push(new InsertCommand());
 }
