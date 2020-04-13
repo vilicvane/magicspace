337385c5348ccdaef5eed8ea3f338f7cf62a6801
ooyyloo <806470946@qq.com>
2020-04-13 10:40:57 +0800

    #2903 add "only-warn" plugin

--- packages/eslint-plugin/src/configs/@override-dev.ts
+++ packages/eslint-plugin/src/configs/@override-dev.ts
@@ -1,7 +1,14 @@
+let plugins = [];
+
+if (process.argv[1] && process.argv[1].includes('dbaeumer.vscode-eslint')) {
+  plugins.push('only-warn');
+}
+
 export const overrideDevConfig = {
+  plugins,
   rules: {
     'import/no-extraneous-dependencies': [
-      'warn',
+      'error',
       {
         devDependencies: true,
       },
