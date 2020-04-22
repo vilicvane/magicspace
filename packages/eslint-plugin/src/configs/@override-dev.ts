a988db71e9d36f7bda147c530a040b76ba6f100d
ooyyloo <806470946@qq.com>
2020-04-22 10:06:35 +0800

    #2903 delete plugins in override-dev

--- packages/eslint-plugin/src/configs/@override-dev.ts
+++ packages/eslint-plugin/src/configs/@override-dev.ts
@@ -1,11 +1,4 @@
-let plugins = [];
-
-if (process.argv[1] && process.argv[1].includes('dbaeumer.vscode-eslint')) {
-  plugins.push('only-warn');
-}
-
 export const overrideDevConfig = {
-  plugins,
   rules: {
     'import/no-extraneous-dependencies': [
       'error',
