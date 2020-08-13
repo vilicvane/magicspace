04c9b39a575c988fbde4110c77381c06c8dca31f
vilicvane <i@vilic.info>
2020-08-14 03:00:31 +0800

    Remove obsolete file context

--- packages/core/src/library/file/context.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Context is shared per destination file.
- */
-export class Context<TMetadata extends object> {
-  // eslint-disable-next-line no-null/no-null
-  readonly metadata = Object.create(null) as TMetadata;
-
-  constructor(readonly path: string) {}
-}
