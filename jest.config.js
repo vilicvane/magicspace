df06a9ed418213b22f316763ddbdf2d897babf54
vilicvane <i@vilic.info>
2020-08-18 09:17:47 +0800

    Fix jest warning

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   clearMocks: true,
   testEnvironment: 'node',
   testMatch: ['<rootDir>/packages/*/bld/test/*.test.js'],
+  modulePathIgnorePatterns: ['<rootDir>/packages/core/.magicspace'],
 };
