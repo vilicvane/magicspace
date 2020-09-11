05fd9c6e20fd7decb09e9d588eead28580c821ae
vilicvane <i@vilic.info>
2020-09-11 21:53:51 +0800

    Add version toggle

--- packages/cli/src/program/commands/default.ts
+++ packages/cli/src/program/commands/default.ts
@@ -1,4 +1,12 @@
-import {SubcommandDefinition} from 'clime';
+import {
+  Command,
+  HelpInfo,
+  Options,
+  SubcommandDefinition,
+  command,
+  metadata,
+  option,
+} from 'clime';
 
 export const subcommands: SubcommandDefinition[] = [
   {name: 'create'},
@@ -6,3 +14,26 @@ export const subcommands: SubcommandDefinition[] = [
   {name: 'update'},
   {name: 'update-dirs'},
 ];
+
+export class DefaultCommandOptions extends Options {
+  @option({
+    toggle: true,
+    flag: 'v',
+  })
+  version!: boolean;
+}
+
+@command({
+  description: 'Create magicspace config file',
+})
+export default class DefaultCommand extends Command {
+  @metadata
+  async execute(options: DefaultCommandOptions): Promise<string | HelpInfo> {
+    if (options.version) {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
+      return require('../../../package').version;
+    }
+
+    return DefaultCommand.getHelp();
+  }
+}
