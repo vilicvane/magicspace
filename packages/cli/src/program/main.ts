877d56a244e2774722f442dcb6cbdf757d15bbe6
vilicvane <vilicvane@live.com>
2021-10-06 20:57:38 +0800

    Add global agent support for latest-version package

--- packages/cli/src/program/main.ts
+++ packages/cli/src/program/main.ts
@@ -2,6 +2,11 @@
 
 import * as Path from 'path';
 
+// eslint-disable-next-line @mufan/import-groups
+process.env.GLOBAL_AGENT_ENVIRONMENT_VARIABLE_NAMESPACE = '';
+
+import 'global-agent/bootstrap';
+
 import {CLI, Shim} from 'clime';
 
 let cli = new CLI('magicspace', Path.join(__dirname, 'commands'));
